8e42139aa5fb4a775d6039868918c065b2ba6a25
Neto Deolino <netodeolino@gmail.com>
2019-03-07 18:28:41 -0300

    mais alguns testes

--- desafios/repeticao/repeticao.test.js
+++ desafios/repeticao/repeticao.test.js
@@ -48,3 +48,48 @@ describe('somatorio()', () => {
     assert.notEqual(somatorio(num), 2)
   })
 })
+
+describe('imparesDe3Ate500()', () => {
+  it('Deve somar impares de 3 a 500 [1]', async () => {
+    assert.equal(imparesDe3Ate500(), 83)
+  })
+
+  it('Deve somar impares de 3 a 500 [2]', async () => {
+    assert.notEqual(imparesDe3Ate500(), 3)
+  })
+
+  it('Deve somar impares de 3 a 500 [3]', async () => {
+    assert.notEqual(imparesDe3Ate500(), 503)
+  })
+})
+
+describe('bacteria()', () => {
+  it('Deve retornar a quantidade de bacterias [1]', async () => {
+    const num = 0
+    assert.equal(bacteria(num), 1)
+  })
+
+  it('Deve retornar a quantidade de bacterias [2]', async () => {
+    const num = 1
+    assert.equal(bacteria(num), 2)
+  })
+
+  it('Deve retornar a quantidade de bacterias [3]', async () => {
+    const num = 13
+    assert.equal(bacteria(num), 8192)
+  })
+})
+
+describe('somaDivisiveis()', () => {
+  it('Deve somar 200 num naturais divisiveis por e não por 7 [1]', async () => {
+    assert.equal(somaDivisiveis(), 5688)
+  })
+
+  it('Deve somar 200 num naturais divisiveis por e não por 7 [2]', async () => {
+    assert.notEqual(somaDivisiveis(), 5687)
+  })
+
+  it('Deve somar 200 num naturais divisiveis por e não por 7 [3]', async () => {
+    assert.notEqual(somaDivisiveis(), 5689)
+  })
+})
